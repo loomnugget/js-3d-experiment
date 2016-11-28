694fa90889b0fb68f48686fd2f24a2081d427bcd
Claudia <claudia.cedfeldt@gmail.com>
2016-11-28 15:19:59 -0800

    updated readme and rendering is working as expected

--- app/polyhedra/kepler-poinsot-mesh.js
+++ app/polyhedra/kepler-poinsot-mesh.js
@@ -5,47 +5,7 @@ const Vector3D = require('../math/vector3d.js');
 const C0 = (3 - Math.sqrt(5)) / 4;
 const C1 = (Math.sqrt(5) - 1) / 4;
 
-const stellation1 = function() {
-  this.vertices = [
-    new Vector3D(0.0, -0.5, C0),
-    new Vector3D( 0.0, -0.5,-C0),
-    new Vector3D( 0.0, 0.5, C0),
-    new Vector3D( 0.0, 0.5, -C0),
-    new Vector3D(-0.5, C0, 0.0),
-    new Vector3D( 0.5, C0, 0.0),
-    new Vector3D(-0.5, -C0, 0.0),
-    new Vector3D( 0.5, -C0, 0.0),
-    new Vector3D(C0, 0.0, -0.5),
-    new Vector3D(C0, 0.0, 0.5),
-    new Vector3D(-C0, 0.0, -0.5),
-    new Vector3D(-C0, 0.0, 0.5),
-  ];
-
-  this.faces = [
-    { A:0, B:2, C:10 },
-    { A:0, B:10, C:5 },
-    { A:0, B:5, C:4 },
-    { A:0, B:4, C:8 },
-    { A:0, B:8, C:2 },
-    { A:3, B:1, C:11 },
-    { A:3, B:11, C:7 },
-    { A:3, B:7, C:6 },
-    { A:3, B:6, C:9 },
-    { A:3, B:9, C:1 },
-    { A:2, B:6, C:7 },
-    { A:2, B:7, C:10 },
-    { A:10, B:7, C:11 },
-    { A:10, B:11, C:5 },
-    { A:5, B:11, C:1 },
-    { A:5, B:1, C:4 },
-    { A:4, B:1, C:9 },
-    { A:4, B:9, C:8 },
-    { A:8, B:9, C:6 },
-    { A:8, B:6, C:2 },
-  ];
-};
-
-const stellation2 = function() {
+const stellation1 = module.exports = function() {
   this.vertices = [
     new Vector3D(0.5, 0.0, C0),
     new Vector3D(0.5, 0.0, -C0),
