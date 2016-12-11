a842e7f3b9801e5868388046c0c6d76d7bea68a9
Claudia <claudia.cedfeldt@gmail.com>
2016-12-11 15:47:24 -0800

    added scss and working on bundle

--- app/index.jsx
+++ app/index.jsx
@@ -10,135 +10,135 @@ const Matrix = require('./math/matrix.js');
 const Vector3D = require('./math/vector3d.js');
 const stellation1 = require('./polyhedra/kepler-poinsot-mesh.js');
 
-class App extends React.Component {
-  componentDidMount(){
-    this.updateCanvas();
-  }
-
-  updateCanvas() {
-    const canvas = this.refs.canvas;
-    const ctx = this.refs.canvas.getContext('2d');
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-
-    const Mesh = function(shape) {
-      this.vertices = shape.vertices;
-      this.faces = shape.faces;
-      this.avgZ = [];
-      this.rotation = new Vector3D(45,45,45);
-      this.position = new Vector3D(0,0,0);
-      this.velocity = new Vector3D(0,0,0);
-      this.acceleration = new Vector3D(0,0,0);
-    };
-
-    Mesh.prototype.getAvgZ = function(face){
-      this.sum = face.z + v2.z + v3.z + v4.z + v5.z;
-      this.avgZ.push(this.sum/5);
-    };
-
-    function sortByZIndex(a, b){
-      return a.z - b.z;
-    };
-
-    const Camera = function() {
-      this.position = new Vector3D(0, 0, 10);
-      this.target = new Vector3D(0, 0, 0);
-      this.up = new Vector3D(0, 1, 0);
-    };
-
-    const Engine = function() {
-      this.centerX = canvas.width/2;
-      this.centerY = canvas.height/2;
-    };
-
-    Engine.prototype.Clear = function(){
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-    };
-
-    Engine.prototype.Project = function(point, transformMatrix){
-      let projected = Vector3D.transformCoordinates(point, transformMatrix);
-      let x = projected.x * 2 * canvas.width + canvas.width/2;
-      let y = -projected.y * 2 * canvas.height + canvas.height/2;
-      let z = point.z;
-      return new Vector3D(x, y, z);
-    };
-
-    Engine.prototype.drawPoint = function(vertex) {
-      ctx.fillStyle = 'rgba(255,255,255,.7)';
-      ctx.fillRect(vertex.x, vertex.y, 1, 1);
-    };
-
-    Engine.prototype.drawLines = function(vertex1, vertex2, vertex3, vertex4, vertex5) {
-      ctx.beginPath();
-      ctx.strokeStyle = '#131313';
-      ctx.moveTo(vertex1.x, vertex1.y); // pick up "pen," reposition
-      ctx.lineTo(vertex2.x, vertex2.y); // draw line from vertex1 to vertex2
-      ctx.lineTo(vertex3.x, vertex3.y); // draw line from vertex2 to vertex3
-      ctx.lineTo(vertex4.x, vertex4.y);
-      ctx.lineTo(vertex5.x, vertex5.y);
-      ctx.closePath(); // connect end to start
-      ctx.stroke(); // outline the shape
-    };
-
-    Engine.prototype.Render = function(camera, meshes) {
-      let viewMatrix = Matrix.LookAtLH(camera.position, camera.target, camera.up);
-      let projectionMatrix = Matrix.PerspectiveFovLH(0.78, canvas.width/canvas.height, .01, 1.0);
-      // Loop through meshes
-      for (let i = 0; i < meshes.length; i++) {
-        let currentMesh = meshes[i];
-
-        let worldMatrix = Matrix.rotationYPR(currentMesh.rotation.y, currentMesh.rotation.x, currentMesh.rotation.z).multiply(Matrix.Translation(currentMesh.position.y, currentMesh.position.x, currentMesh.position.z));
-        // Final matrix to be applied to each vertex
-        let transformMatrix = worldMatrix.multiply(viewMatrix).multiply(projectionMatrix);
-        // Loop through faces in each mesh
-        for(let i = 0; i < currentMesh.faces.length; i++) {
-          let face = currentMesh.faces[i];
-          // Create each triangular face using indexes from faces array
-          let vertexA = currentMesh.vertices[face.A];
-          let vertexB = currentMesh.vertices[face.B];
-          let vertexC = currentMesh.vertices[face.C];
-          let vertexD = currentMesh.vertices[face.D];
-          let vertexE = currentMesh.vertices[face.E];
-
-          // Project each vertex in the face by applying transformation matrix to all points
-          let projectedVertexA = this.Project(vertexA, transformMatrix);
-          let projectedVertexB = this.Project(vertexB, transformMatrix);
-          let projectedVertexC = this.Project(vertexC, transformMatrix);
-          let projectedVertexD = this.Project(vertexD, transformMatrix);
-          let projectedVertexE = this.Project(vertexE, transformMatrix);
-
-          //face.getAvgZ(projectedVertexA, projectedVertexB, projectedVertexC, projectedVertexD, projectedVertexE);
-          //console.log(currentMesh.avgZ);
-          //Draw Triangles
-          this.drawLines(projectedVertexA, projectedVertexB, projectedVertexC, projectedVertexD, projectedVertexE);
-        }
-      }
-    };
-
-    let meshes = [], camera, engine, mesh, avgZ = [];
-    function init() {
-      camera = new Camera();
-      engine = new Engine();
-      let testShape = new stellation1();
-      mesh = new Mesh(testShape);
-      meshes.push(mesh);
-      requestAnimationFrame(drawingLoop);
-
-    }
-    init();
-
-    //Rendering loop handler
-    function drawingLoop() {
-      engine.Clear();
-      mesh.rotation.x += 0.01;
-      mesh.rotation.y += 0.01;
-      engine.Render(camera, meshes);
-      requestAnimationFrame(drawingLoop);
-    }
-  }
-
-  render() {
-    return <canvas ref="canvas"></canvas>;
-  }
-}
-render(<App/>, document.getElementById('app'));
+// class App extends React.Component {
+//   componentDidMount(){
+//     this.updateCanvas();
+//   }
+//
+//   updateCanvas() {
+//     const canvas = this.refs.canvas;
+//     const ctx = this.refs.canvas.getContext('2d');
+//     canvas.width = window.innerWidth;
+//     canvas.height = window.innerHeight;
+//
+//     const Mesh = function(shape) {
+//       this.vertices = shape.vertices;
+//       this.faces = shape.faces;
+//       this.avgZ = [];
+//       this.rotation = new Vector3D(45,45,45);
+//       this.position = new Vector3D(0,0,0);
+//       this.velocity = new Vector3D(0,0,0);
+//       this.acceleration = new Vector3D(0,0,0);
+//     };
+//
+//     Mesh.prototype.getAvgZ = function(face){
+//       this.sum = face.z + v2.z + v3.z + v4.z + v5.z;
+//       this.avgZ.push(this.sum/5);
+//     };
+//
+//     function sortByZIndex(a, b){
+//       return a.z - b.z;
+//     };
+//
+//     const Camera = function() {
+//       this.position = new Vector3D(0, 0, 10);
+//       this.target = new Vector3D(0, 0, 0);
+//       this.up = new Vector3D(0, 1, 0);
+//     };
+//
+//     const Engine = function() {
+//       this.centerX = canvas.width/2;
+//       this.centerY = canvas.height/2;
+//     };
+//
+//     Engine.prototype.Clear = function(){
+//       ctx.clearRect(0, 0, canvas.width, canvas.height);
+//     };
+//
+//     Engine.prototype.Project = function(point, transformMatrix){
+//       let projected = Vector3D.transformCoordinates(point, transformMatrix);
+//       let x = projected.x * 2 * canvas.width + canvas.width/2;
+//       let y = -projected.y * 2 * canvas.height + canvas.height/2;
+//       let z = point.z;
+//       return new Vector3D(x, y, z);
+//     };
+//
+//     Engine.prototype.drawPoint = function(vertex) {
+//       ctx.fillStyle = 'rgba(255,255,255,.7)';
+//       ctx.fillRect(vertex.x, vertex.y, 1, 1);
+//     };
+//
+//     Engine.prototype.drawLines = function(vertex1, vertex2, vertex3, vertex4, vertex5) {
+//       ctx.beginPath();
+//       ctx.strokeStyle = '#131313';
+//       ctx.moveTo(vertex1.x, vertex1.y); // pick up "pen," reposition
+//       ctx.lineTo(vertex2.x, vertex2.y); // draw line from vertex1 to vertex2
+//       ctx.lineTo(vertex3.x, vertex3.y); // draw line from vertex2 to vertex3
+//       ctx.lineTo(vertex4.x, vertex4.y);
+//       ctx.lineTo(vertex5.x, vertex5.y);
+//       ctx.closePath(); // connect end to start
+//       ctx.stroke(); // outline the shape
+//     };
+//
+//     Engine.prototype.Render = function(camera, meshes) {
+//       let viewMatrix = Matrix.LookAtLH(camera.position, camera.target, camera.up);
+//       let projectionMatrix = Matrix.PerspectiveFovLH(0.78, canvas.width/canvas.height, .01, 1.0);
+//       // Loop through meshes
+//       for (let i = 0; i < meshes.length; i++) {
+//         let currentMesh = meshes[i];
+//
+//         let worldMatrix = Matrix.rotationYPR(currentMesh.rotation.y, currentMesh.rotation.x, currentMesh.rotation.z).multiply(Matrix.Translation(currentMesh.position.y, currentMesh.position.x, currentMesh.position.z));
+//         // Final matrix to be applied to each vertex
+//         let transformMatrix = worldMatrix.multiply(viewMatrix).multiply(projectionMatrix);
+//         // Loop through faces in each mesh
+//         for(let i = 0; i < currentMesh.faces.length; i++) {
+//           let face = currentMesh.faces[i];
+//           // Create each triangular face using indexes from faces array
+//           let vertexA = currentMesh.vertices[face.A];
+//           let vertexB = currentMesh.vertices[face.B];
+//           let vertexC = currentMesh.vertices[face.C];
+//           let vertexD = currentMesh.vertices[face.D];
+//           let vertexE = currentMesh.vertices[face.E];
+//
+//           // Project each vertex in the face by applying transformation matrix to all points
+//           let projectedVertexA = this.Project(vertexA, transformMatrix);
+//           let projectedVertexB = this.Project(vertexB, transformMatrix);
+//           let projectedVertexC = this.Project(vertexC, transformMatrix);
+//           let projectedVertexD = this.Project(vertexD, transformMatrix);
+//           let projectedVertexE = this.Project(vertexE, transformMatrix);
+//
+//           //face.getAvgZ(projectedVertexA, projectedVertexB, projectedVertexC, projectedVertexD, projectedVertexE);
+//           //console.log(currentMesh.avgZ);
+//           //Draw Triangles
+//           this.drawLines(projectedVertexA, projectedVertexB, projectedVertexC, projectedVertexD, projectedVertexE);
+//         }
+//       }
+//     };
+//
+//     let meshes = [], camera, engine, mesh, avgZ = [];
+//     function init() {
+//       camera = new Camera();
+//       engine = new Engine();
+//       let testShape = new stellation1();
+//       mesh = new Mesh(testShape);
+//       meshes.push(mesh);
+//       requestAnimationFrame(drawingLoop);
+//
+//     }
+//     init();
+//
+//     //Rendering loop handler
+//     function drawingLoop() {
+//       engine.Clear();
+//       mesh.rotation.x += 0.01;
+//       mesh.rotation.y += 0.01;
+//       engine.Render(camera, meshes);
+//       requestAnimationFrame(drawingLoop);
+//     }
+//   }
+//
+//   render() {
+//     return <canvas ref="canvas"></canvas>;
+//   }
+// }
+// render(<App/>, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -2,18 +2,23 @@
 
 const webpack = require('webpack');
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
-  template: __dirname + '/app/index.html',
-  filename: 'index.html',
-  inject: 'body',
-});
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+let plugins = [
+  new ExtractTextPlugin('bundle.css'),
+  new HTMLWebpackPlugin({template: `${__dirname}/app/index.html`}),
+];
 
 module.exports = {
   entry: `${__dirname}/app/index.js`,
+  plugins,
   output: {
     path: 'build',
     filename: 'bundle.js',
+  },
+  sassLoader: {
+    includePaths: [`${__dirname}/app/scss/lib`],
   },
   module : {
     loaders : [
@@ -26,7 +31,22 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         loader: 'babel'},
+      {
+        test: /\.html$/,
+        loader: 'html',
+      },
+      {
+        test: /\.(woff|ttf|svg|otf|eot).*/,
+        loader: 'url?limit=10000&name=font/[name].[ext]',
+      },
+      {
+        test: /\.(jpg|jpeg|bmp|tiff|gif|png)$/,
+        loader: 'url?limit=10000&name=image/[hash].[ext]',
+      },
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract('style', 'css!resolve-url!sass?sourceMap'),
+      },
     ],
   },
-  plugins: [HTMLWebpackPluginConfig],
 };
